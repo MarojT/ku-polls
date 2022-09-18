7d536236f0b123dacd934d4e5bf6288a7ba84ad4
Maroj Thangthong <celta0992@hotmail.com>
2022-09-18 23:51:02 +0700

    add logout login featured

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/polls/'    # show list of polls
+LOGIN_REDIRECT_URL = '/polls/'  # show list of polls
 
-LOGOUT_REDIRECT_URL = '/'         # after logout, go where?
+LOGOUT_REDIRECT_URL = '/'  # after logout, go to login.
